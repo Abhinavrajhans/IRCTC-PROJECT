a24c7d65d4bda8eb6539e446747bbdcf24122c7f
abhinavrajhans <abhinav.rajhans1@gmail.com>
2025-08-29 17:06:25 +0530

    added data integrity violation exception

--- src/main/java/com/example/irctc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/irctc/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.irctc.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -39,14 +40,28 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
     }
 
+
+    @ExceptionHandler(org.springframework.dao.DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex){
+        String message="Duplicate entry: The value you are trying to save already exists";
+
+        ErrorResponse err= ErrorResponse.builder()
+                .status(HttpStatus.CONFLICT.value())
+                .message(message)
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(err, HttpStatus.CONFLICT);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
         ErrorResponse err = ErrorResponse.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .message("An unexpected error occurred. Please contact support.")
+                .message("An unexpected error occurred. Please contact support. The Exception is : " +  ex.getClass().getName())
                 .timestamp(LocalDateTime.now())
                 .build();
 
         return new ResponseEntity<>(err, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
