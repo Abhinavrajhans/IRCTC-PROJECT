a967b8d1c07e69a979dae16e14a89345ced52001
abhinavrajhans <abhinav.rajhans1@gmail.com>
2025-08-29 20:29:48 +0530

    removed the duplicated check

--- src/main/java/com/example/irctc/repository/TrainBodyRepository.java
+++ src/main/java/com/example/irctc/repository/TrainBodyRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TrainBodyRepository extends JpaRepository<TrainBody,Long> {
-    boolean existsByCompanyNameIgnoreCase(String companyName);
 }
--- src/main/java/com/example/irctc/services/TrainBodyService.java
+++ src/main/java/com/example/irctc/services/TrainBodyService.java
@@ -6,7 +6,6 @@ import com.example.irctc.entity.TrainBody;
 import com.example.irctc.exception.TrainBodyNotFoundException;
 import com.example.irctc.mappers.TrainBodyMapper;
 import com.example.irctc.repository.TrainBodyRepository;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -23,11 +22,6 @@ public class TrainBodyService implements ITrainBodyService {
 
     @Override
     public TrainBodyResponseDTO createTrainBody(TrainBodyRequestDTO trainBodyDTO)  {
-        String companyName=trainBodyDTO.getCompanyName().trim();
-        if(this.trainBodyRepository.existsByCompanyNameIgnoreCase(companyName))
-        {
-            throw new DataIntegrityViolationException("Company name already exists");
-        }
         TrainBody trainbody = TrainBodyMapper.toEntity(trainBodyDTO);
         TrainBody savedTrainBody = trainBodyRepository.save(trainbody);
         return TrainBodyMapper.toResponseDTO(savedTrainBody);
